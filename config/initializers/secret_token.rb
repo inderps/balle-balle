9591bbfc35c25fe758f1c7ccc8887044c10fc1cd
Inderpal Singh <Inderpal@IPSmac-2.local>
2012-11-02 02:38:05 +0530

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BalleBalle::Application.config.secret_token = '2c03d1dec9c3316dacf6badbe863635c255027aeae316d79ab3be7d7c31736b0486e1121d07fb90a853a7e450a03c6844fde3b832e87fb19357da6dbd1308246'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BalleBalle::Application.config.session_store :cookie_store, key: '_balle-balle_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BalleBalle::Application.config.session_store :active_record_store
