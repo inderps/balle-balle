b172993768e6a814d5203ad4e6813a45b7949f7b
Inderpal Singh <Inderpal@IPSmac-2.local>
2012-11-02 04:34:27 +0530

    ui styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,21 +24,16 @@ var BalleBalle = {
                 $.each(songs, function(index, song) {
                     $("#songTemplate").tmpl(song).appendTo("#songs");
                     if (toggle == false) {
-                        $("div[data-id=" + song.id + "]").addClass("grey-row");
-                        $("div[data-id=" + song.id + "]").removeClass("white-row");
+                        $("tr[data-id=" + song.id + "]").addClass("grey-row");
+                        $("tr[data-id=" + song.id + "]").removeClass("white-row");
                         toggle = true;
                     }
                     else {
-                        $("div[data-id=" + song.id + "]").addClass("white-row");
-                        $("div[data-id=" + song.id + "]").removeClass("grey-row");
+                        $("tr[data-id=" + song.id + "]").addClass("white-row");
+                        $("tr[data-id=" + song.id + "]").removeClass("grey-row");
                         toggle = false;
                     }
-//                    $("#movie-default .movie-row .movie").html(movie.title);
-//                    $("#movie-default .movie-row").attr("data-id", movie.id);
-//                    var movie_row = $("#movie-default").html();
-//                    $("#movies-list").append(movie_row);
                 });
-//                $("#movies-list .movie-row").show();
 //                bind_events();
             }
         });
