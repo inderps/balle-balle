a6b0c7c86dcd63f42cd24aa70c421191408c7a32
Inderpal Singh <Inderpal@IPSmac-2.local>
2012-11-02 04:03:18 +0530

    now it fetches songs list via ajax on home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,48 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require jquery-tmpl
+
+var BalleBalle = {
+    load_songs: function() {
+        $.ajax({
+            url: "songs",
+            type: "get",
+            success: function(songs) {
+                var toggle = false;
+                $.each(songs, function(index, song) {
+                    $("#songTemplate").tmpl(song).appendTo("#songs");
+                    if (toggle == false) {
+                        $("div[data-id=" + song.id + "]").addClass("grey-row");
+                        $("div[data-id=" + song.id + "]").removeClass("white-row");
+                        toggle = true;
+                    }
+                    else {
+                        $("div[data-id=" + song.id + "]").addClass("white-row");
+                        $("div[data-id=" + song.id + "]").removeClass("grey-row");
+                        toggle = false;
+                    }
+//                    $("#movie-default .movie-row .movie").html(movie.title);
+//                    $("#movie-default .movie-row").attr("data-id", movie.id);
+//                    var movie_row = $("#movie-default").html();
+//                    $("#movies-list").append(movie_row);
+                });
+//                $("#movies-list .movie-row").show();
+//                bind_events();
+            }
+        });
+    }
+//
+//    vote: function(url, vote, movie_id) {
+//        $.ajax({
+//            url: url,
+//            type: "post",
+//            data: { "vote" : vote, "movie_id" : movie_id },
+//            success: function(response) {
+//            }
+//        });
+//    },
+//    vote_current_movie: function(vote, movie_id) {
+//        this.vote("current_movies/vote", vote, movie_id);
+//    }
+}
