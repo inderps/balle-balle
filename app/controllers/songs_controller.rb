87b3b3be7a3727d28619edf9f652e347623d7a63
Inderpal Singh <Inderpal@ipsmac.(none)>
2012-11-06 09:12:35 +0530

    added the action for adding songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,15 @@
 class SongsController < ApplicationController
+  skip_before_filter :authorize
+  
   def index
     user = session[:user]
     songs = Song.select("songs.*, v.vote as vote").joins("left join votes v on songs.id = v.song_id and v.user_id = #{user.id}")
     songs.sort_by! {|k, v| k.up_votes - k.down_votes}.reverse!
     render :json => songs
+  end
+
+  def create
+    song = Song.create(:title => params[:title], :artist => params[:artist], :album => params[:album], :link => params[:link])
+    render :json => song
   end
 end
